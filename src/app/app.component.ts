133eb57e489fb998d17da56fd5f288e5734ab503
moby1804 <mogbitseo@yahoo.com>
2023-12-04 23:31:57 +0000

    initial app creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './auth/login/login.component';
+import { SideNavComponent } from './app-core/common/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule, LoginComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnChanges, OnInit {
   title = 'inmest-web';
+  @Input()  name = "Lucky";
+
+  profile = {
+id: 324,
+firstName: "Lucky",
+lastName: "Dogbe",
+  }
+  genesis: string = "hello";
+
+  constructor(){
+    console.log("constructor");
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes, 'changes');
+    // console.log(this.name, 'OnChanges');
+    for(const inputChange in changes){
+      console.log(changes[inputChange], inputChange);
+
+
+    }
+  }
+
+  ngOnInit(): void {
+      console.log('On Init');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './main-features/dashboard/dashboard.component';
+import { UserComponent } from './main-features/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '',
+component: DashboardComponent,
+},
+
+{path: 'user',
+component: UserComponent,
+},
+
+
+];
